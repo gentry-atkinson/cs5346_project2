8cc7395bc511d794500dbbb8a822047858d38c87
vishalkumaar <vishal.kumaar123@gmail.com>
2018-10-29 16:09:14 -0500

    Added few moves : Test purpose

--- Kalah/main.cpp
+++ Kalah/main.cpp
@@ -11,5 +11,11 @@ int main()
     board.draw();
     board.move(10);
     board.draw();
+    board.move(3);
+    board.draw();
+    board.move(11);
+    board.draw();
+    board.move(4);
+    board.draw();
     return 0;
 }

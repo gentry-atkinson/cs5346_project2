b48a5c4c85860970ef029beff81b99ad5607c288
vishalkumaar <vishal.kumaar123@gmail.com>
2018-11-11 20:20:18 -0600

    Updated the values of board

--- Kalah/include/Board.h
+++ Kalah/include/Board.h
@@ -4,30 +4,30 @@
 
 class Board
 {
-    public:
-        Board();
-        Board(const Board& toCopy);
-        ~Board();
-        void draw();
-        int move(int hole, int player);
-        void setValue(int algorithm, int player);
-        void setValue (int newValue);
-        int getValue();
-        bool isLegal(int moveNumber, int player);
-        bool isFinished();
-        int getScore1();
-        int getScore2();
-        Board& operator= (const Board& other);
-
-    protected:
-
-    private:
-        int holes[14]= {0, 6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 6, 6};
-        int value;
-        bool finished;
-
-        int gentryValue(int player);
-        int vishalValue();
+public:
+    Board();
+    Board(const Board& toCopy);
+    ~Board();
+    void draw();
+    int move(int hole, int player);
+    void setValue(int algorithm, int player);
+    void setValue (int newValue);
+    int getValue();
+    bool isLegal(int moveNumber, int player);
+    bool isFinished();
+    int getScore1();
+    int getScore2();
+    Board& operator= (const Board& other);
+    
+protected:
+    
+private:
+    int holes[14]= {6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 6, 6, 0};
+    int value;
+    bool finished;
+    
+    int gentryValue(int player);
+    int vishalValue(int player);
 };
 
 #endif // BOARD_H

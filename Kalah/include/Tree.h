0c74c4b7118e27a4cb6961cc010ffdf098256ba7
vishalkumaar <vishal.kumaar123@gmail.com>
2018-11-15 00:34:07 -0600

    Code written for A-B Search and added necessary functions for it to work efficiently

--- Kalah/include/Tree.h
+++ Kalah/include/Tree.h
@@ -10,28 +10,35 @@
 
 class Tree
 {
-    public:
-        Tree();
-        Tree(int value, int search, int depth, int player);
-        ~Tree();
-        int play(int lastMove, bool& finished, int& player);
-        void drawRoot();
-
-    private:
-
-        int depth;
-        int player;
-        int valueAlgorithm;
-        int searchAlgorithm;
-        Board * boards;
-        int totalBoards;
-
-        int getChildIndex(int parentIndex, int childNumber);
-        int getParentIndex(int childIndex);
-        void buildTree();
-        int minMaxAB(int index, int depth, int player, int useThresh, int passThresh);
-        int aBSearch();
-
+public:
+    int value;
+    Tree();
+    Tree(int value, int search, int depth, int player);
+    ~Tree();
+    int play(int lastMove, bool& finished, int& player);
+    void drawRoot();
+    Tree *children[14];
+    Board bx;
+    
+    
+    //private:
+    
+    int depth;
+    int player;
+    int valueAlgorithm;
+    int searchAlgorithm;
+    Board * boards;
+    int totalBoards;
+    int getChildIndex(int parentIndex, int childNumber);
+    int getParentIndex(int childIndex);
+    void buildTree();
+    int minMaxAB(int index, int depth, int player, int useThresh, int passThresh);
+    void setValue(int );
+    bool IsitDeep(int );
+    int evaluation();
+    int alphabeta(Tree *node, int depth, int player, int alpha, int beta);
+    
+    
 };
 
 #endif // TREE_H
--- Kalah/src/Tree.cpp
+++ Kalah/src/Tree.cpp
@@ -9,11 +9,15 @@ Tree::Tree()
     depth = 3;
     player = 1;
     valueAlgorithm = 1;
+    value=-1000;
     searchAlgorithm = 1;
     totalBoards = 259;
-
+    for(int i=0;i<6;i++)
+    {
+        children[i] = NULL;
+    }
     boards = new Board[totalBoards];
-
+    
     buildTree();
 }
 
@@ -28,13 +32,11 @@ Tree::Tree(int value, int search, int depth, int player){
     valueAlgorithm = value;
     searchAlgorithm = search;
     this->player = player;
-
     totalBoards = 1;
     for (int i = 1; i <= depth; i++)
         totalBoards += pow(6, i);
     boards = new Board[totalBoards];
     //cout << "Player " << player << " has " << totalBoards << " boards in tree." << endl;
-
     buildTree();
 }
 
@@ -54,7 +56,7 @@ void Tree::drawRoot(){
 void Tree::buildTree(){
     //cout << "Building tree for Player " << player << endl;
     int child;
-
+    
     for (int i = 0; i < depth; i++){
         for (int j = 1; j <= 6; j++){
             child = 6*i+j;
@@ -64,19 +66,19 @@ void Tree::buildTree(){
         }
     }
     cout << "Tree done." << endl;
-
+    
     switch(searchAlgorithm){
-    case 1:
-        minMaxAB(0, 0, player, 100, -120);
-        break;
-    case 2:
-        void aBSearch();
-        break;
-    default:
-        cerr << "Bad value for search algorithm in buildTree" << endl;
-        break;
+        case 1:
+            minMaxAB(0, 0, player, 100, -120);
+            break;
+        case 2:
+            void aBSearch();
+            break;
+        default:
+            cerr << "Bad value for search algorithm in buildTree" << endl;
+            break;
     }
-
+    
     return;
 }
 
@@ -84,103 +86,167 @@ int Tree::minMaxAB(int index, int depth, int player, int useThresh, int passThre
     cout << "MinMax called for depth " << depth << endl;
     int newValue;
     int resultSucc;
-//Algorithm: MINIMAX-A-B( Position, Depth, Player, Use-Thresh, Pass-Thresh )
-//1. If DEEP-ENOUGH(Position, Depth), then return the structure
+    //Algorithm: MINIMAX-A-B( Position, Depth, Player, Use-Thresh, Pass-Thresh )
+    //1. If DEEP-ENOUGH(Position, Depth), then return the structure
     if (depth >= this->depth){
-//VALUE = STATlC (Position, Player);
+        //VALUE = STATlC (Position, Player);
         boards[index].setValue(valueAlgorithm, player);
         return boards[index].getValue();
     }
-//PATH = nil
-//2. Otherwise, generate one more ply of the tree by calling the function MOVE- 	GEN(Position, Player) and setting SUCCESSORS to the list it returns.
-
-//3.If SUCCESSORS is empty, there are no moves to be made; return the same structure that 	would have been returned if DEEP-ENOUGH had returned TRUE.
-
-//4. If SUCCESSORS is not empty, then go through it, examining each element and keeping 	track of the best one. This is done as follows.
-//For each clement SUCC of SUCCESSORS:
+    //PATH = nil
+    //2. Otherwise, generate one more ply of the tree by calling the function MOVE-     GEN(Position, Player) and setting SUCCESSORS to the list it returns.
+    
+    //3.If SUCCESSORS is empty, there are no moves to be made; return the same structure that     would have been returned if DEEP-ENOUGH had returned TRUE.
+    
+    //4. If SUCCESSORS is not empty, then go through it, examining each element and keeping     track of the best one. This is done as follows.
+    //For each clement SUCC of SUCCESSORS:
     for (int i = 0; i < 6; i++){
-//(a) Set RESULT-SUCC to
-//MINIMAX-A-B(SUCC, Depth + 1, OPPOSlTE (Player),
-//- Pass-Thresh, - Use-Thresh).
+        //(a) Set RESULT-SUCC to
+        //MINIMAX-A-B(SUCC, Depth + 1, OPPOSlTE (Player),
+        //- Pass-Thresh, - Use-Thresh).
         resultSucc = minMaxAB(getChildIndex(index, i), depth+1, player, -1 * passThresh, -1 * useThresh);
-//(b) Set NEW-VALUE to - VALUE(RESULT-SUCC).
+        //(b) Set NEW-VALUE to - VALUE(RESULT-SUCC).
         newValue = -1 * resultSucc;
-//(c) If NEW-VALUE> Pass-Thresh, then we have found a successor that is better than any that have
-//been examined so far. Record this by doing the following.
+        //(c) If NEW-VALUE> Pass-Thresh, then we have found a successor that is better than any that have
+        //been examined so far. Record this by doing the following.
         if (newValue > passThresh){
-//(i) Set Pass-Thresh to NEW-VALUE.
+            //(i) Set Pass-Thresh to NEW-VALUE.
             passThresh = newValue;
-//(ii) The best known path is now from CURRENT to SUCC and then on to the appropriate path
-//from SUCC as determined by the recursive call to MINIMAX-A-B. So set BEST-PATH to
-//the result of attaching SUCC to the front of PATH(RESULT-SUCC).
+            //(ii) The best known path is now from CURRENT to SUCC and then on to the appropriate path
+            //from SUCC as determined by the recursive call to MINIMAX-A-B. So set BEST-PATH to
+            //the result of attaching SUCC to the front of PATH(RESULT-SUCC).
         }
-//(d) If Pass-Thresh (reflecting the current best value) is not better than Use-Thresh, then we should
-//stop examining this branch. But both thresholds and values have been inverted. So if Pass-Thresh
-//>= Use-Thresh, then return immediately with the value
-// VALUE = Pass-Thresh
-// PATH = BEST-PATH
+        //(d) If Pass-Thresh (reflecting the current best value) is not better than Use-Thresh, then we should
+        //stop examining this branch. But both thresholds and values have been inverted. So if Pass-Thresh
+        //>= Use-Thresh, then return immediately with the value
+        // VALUE = Pass-Thresh
+        // PATH = BEST-PATH
         if (passThresh <= useThresh)
             return passThresh;
     }
-//5. Return the structure
-//VALUE = Pass-Thresh
-//PATH = BEST-PATH
+    //5. Return the structure
+    //VALUE = Pass-Thresh
+    //PATH = BEST-PATH
     return passThresh;
+    
+}
 
+void Tree::setValue(int value)
+{
+    value = value;
+}
+
+bool Tree::IsitDeep(int depth)
+{
+    if(value != -1000)
+        return value;
+    //if the depth is greater than 3 or a player has won the game then it is deep enough.
+    if(depth >= 3 || bx.isFinished())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int Tree::evaluation()
+{
+    int p1stones=0, p2stones=0;
+    int value = 0;
+    if(player == 1)
+    {
+        int p1stones = 0;
+        for(int i = 0 ; i < 6 ; i++)
+        {
+            if(bx.holes[i] == 0)
+                p1stones++;
+        }
+        if(p1stones == 6)
+            value = 1000;
+    }
+    else if(player == 2)
+    {
+        int p2stones = 0;
+        for(int i = 7 ; i < 14 ; i++)
+        {
+            if(bx.holes[i] == 0)
+                p2stones++;
+        }
+        if(p2stones == 6)
+            value = -1000;
+    }
+    setValue(value);
+    return value;
 }
 
 
-int Tree::aBSearch(){
-//    function ALPHA-BETA-SEARCH(state) returns an action
-//    v ←MAX-VALUE(state,−∞,+∞)
-//    return the action in ACTIONS(state) with value v
-
-//    function MAX-VALUE(state, α, β) returns a utility value
-//    if TERMINAL-TEST(state) then return UTILITY(state)
-//    v ← −∞
-//    for each a in ACTIONS(state) do
-//    v ← MAX(v, MIN-VALUE(RESULT(s,a), α, β))
-//    if v ≥ β then return v
-//    α←MAX(α, v)
-//    return v
-
-//    function MIN-VALUE(state, α, β) returns a utility value
-//    if TERMINAL-TEST(state) then return UTILITY(state)
-//    v ← +∞
-//    for each a in ACTIONS(state) do
-//    v ← MIN(v, MAX-VALUE(RESULT(s,a) , α, β))
-//    if v ≤ α then return v
-//    β ← MIN(β, v)
-//  return v
-
-
+int alphabeta(Tree *node, int depth, int player, int alpha, int beta)
+{
+    //Initial Condition -> node is a leaf node
+    if(node->IsitDeep(depth))
+        return node->evaluation() ;
+    if (player == 1 )
+    {
+        int bestVal = -100,value;
+        for(int i=0;i < 6 ; i++ )
+        {
+            if(node->children[i]== NULL)
+                continue;
+            value = alphabeta(node->children[i], depth+1, node->player, alpha, beta);
+            bestVal = ( bestVal > value) ? bestVal : value;
+            alpha = ( alpha > bestVal) ? alpha : bestVal;
+            if (beta <= alpha)
+                break;
+        }
+        node->setValue(bestVal);
+        return bestVal;
+    }
+    else
+    {
+        int bestVal = +100,value;
+        for(int i=7; i < 14 ; i++ )
+        {
+            if(node->children[i]== NULL)
+                continue;
+            value = alphabeta(node->children[i], depth+1, node->player, alpha, beta);
+            bestVal = ( bestVal < value) ? bestVal : value;
+            beta = ( beta < bestVal) ? beta : bestVal;
+            if (beta < alpha)
+                break;
+        }
+        node->setValue(bestVal);
+        return bestVal;
+    }
 }
+
 
 //if same player's move
-    //select best move from 6 children
-    //update tree
-    //update player
-    //update finished
-    //return best move
+//select best move from 6 children
+//update tree
+//update player
+//update finished
+//return best move
 //if opposite player
-    //update tree
+//update tree
 //TODO: players are making illegal moves and always making the same move.
 int Tree::play(int lastMove, bool& finished, int& player){
     cout << "Player " << player << " is moving." << endl;
     int bestMove = 1;
     if (player == 2)  bestMove = 8;
-
+    
     int i = 1, bound = 7;
     if (player == 2){
         i = 7;
         bound = 14;
     }
-
+    
     if (lastMove != 99){
         boards[0].move(lastMove, player);
         buildTree();
     }
-
+    
     if (player == this->player){
         for (; i < bound; i++){
             cout << i << " ";
